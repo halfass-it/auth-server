adc7f6737aabcc87e01ee3abc5c34fdf4f0d954d
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-18 10:49:25 +0100

    sync

--- auth_server/auth_server.py
+++ auth_server/auth_server.py
@@ -1,8 +1,15 @@
 from utils.filesystem import CacheDir
+from utils.logger import Logger
 from auth_server.auth import Auth
 
 
 class AuthServer:
-  def __init__(self) -> None:
-    self.cache_dir = CacheDir()
+  def __init__(self, ip: str, port: int, buffer_size: int, timeout: int, cache_dir: Path = None) -> None:
+    self.ip: str = ip
+    self.port: int = port
+    self.buffer_size: int = buffer_size
+    self.timeout: int = timeout
+    name: str = str(__name__.split('.')[-1])
+    logs_dir: Path = Path(str(CacheDir())) / 'logs' if not cache_dir else cache_dir
+    self.logger: Logger = Logger(name=name, logs_dir=logs_dir)()
     self.auth = Auth()
